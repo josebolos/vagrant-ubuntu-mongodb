57670798e22d34b5c804351b2e5ad07cc63f64a3
Jose Bolos <joseluisbolos@gmail.com>
2016-05-05 15:23:00 +0100

    Makes mongo accessible outside the VM

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,7 +5,16 @@ package { 'mongodb' :
 
 service { 'mongodb' :
     require => [
-        Package['mongodb']
+        Package['mongodb'],
+        File_line['listen-to-all']
     ],
     ensure => running
+}
+
+
+file_line { 'listen-to-all':
+    path => '/etc/mongodb.conf',
+    match => '^.*bind_ip = 127.0.0.1$',
+    line => 'bind_ip = 0.0.0.0',
+    require => Package['mongodb']
 }
